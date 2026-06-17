9a583ae2a99361c4144c732b38436750ead2a425
Miguel Cuesta <miguelcuesma@gmail.com>
2026-06-18 01:22:00 +0200

    Fixed static export (only index could be shown before)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import wasm from 'vite-plugin-wasm'
+import { resolve } from 'path'
 
 const isGithubPages = process.env.GITHUB_PAGES === 'true'
-const repoName = process.env.REPO_NAME || 'tfg-code'
+const repoName = process.env.REPO_NAME || 'tfg_code'
 
 export default defineConfig({
   base: isGithubPages ? `/${repoName}/` : './',
@@ -14,5 +15,11 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'static/index.html'),
+        about: resolve(__dirname, 'static/about.html'),
+      },
+    },
   },
 })
